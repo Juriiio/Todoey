7e8e1f942554c862d856a4ee401209b4d5f9bdec
Jürgen Geider <j.geider91@me.com>
2019-01-30 18:24:03 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jürgen Geider on 1/30/19.
+//  Copyright © 2019 Juriiio. All rights reserved.
+//
+
+import Foundation
+
+    
+class Item {
+    var titel: String = ""
+    var done: Bool = false
+    
+}
